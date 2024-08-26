a0c5c16c6761587b8912dcadbc24d4a25e21f929
Abbas khan <abbaszusaman@gmail.com>
2024-08-26 10:54:48 +0500

    Add for car Delete And Get By ID

--- CarRental/CRCQRS.API/Controllers/CarController.cs
+++ CarRental/CRCQRS.API/Controllers/CarController.cs
@@ -2,55 +2,66 @@ using CRCQRS.Application.Commands;
 using CRCQRS.Application.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CRCQRS.API.Controllers
 {
   //[Authorize]
   [Route("api/[controller]")]
   [ApiController]
-  public class CarController : BaseApi
+  public class CarController : ControllerBase
   {
-    public CarController(IMediator mediator) : base(mediator)
+    private readonly IMediator _mediator;
+
+    public CarController(IMediator mediator)
     {
+      _mediator = mediator;
     }
 
     [HttpPost("Add")]
-    public async Task<IActionResult> Add(RegisterCarCommand command)
+    public async Task<IActionResult> Add([FromBody] RegisterCarCommand command)
     {
       if (!ModelState.IsValid)
       {
         return BadRequest(ModelState);
       }
-      return await RunCommand(command);
+      var result = await _mediator.Send(command);
+      return Ok(result);
     }
+
     [HttpPut("Update")]
-    public async Task<IActionResult> Update(UpdateCarCommand command)
+    public async Task<IActionResult> Update([FromBody] UpdateCarCommand command)
     {
       if (!ModelState.IsValid)
       {
         return BadRequest(ModelState);
       }
-      return await RunCommand(command);
+      var result = await _mediator.Send(command);
+      return Ok(result);
     }
-    [HttpDelete("Delete")]
-    public async Task<IActionResult> Delete(DeleteCarCommand command)
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<IActionResult> Delete(int id)
     {
-      if (!ModelState.IsValid)
-      {
-        return BadRequest(ModelState);
-      }
-      return await RunCommand(command);
+      var command = new DeleteCarCommand { CarId = id };
+      var result = await _mediator.Send(command);
+      return Ok(result);
     }
+
     [HttpGet("GetAll")]
     public async Task<IActionResult> GetAll()
     {
-      return await RunCommand(new GetAllCarsQuery());
+      var query = new GetAllCarsQuery();
+      var result = await _mediator.Send(query);
+      return Ok(result);
     }
-    [HttpGet("GetById{id}")]
-    public async Task<IActionResult> GetById(GetCarsByIdQuery command)
+
+    [HttpGet("GetById/{id}")]
+    public async Task<IActionResult> GetById(int id)
     {
-     
-      return await RunCommand(command);
+      var query = new GetCarsByIdQuery { CarId = id };
+      var result = await _mediator.Send(query);
+      return Ok(result);
     }
   }
 }
--- CarRental/CRCQRS.Application/Queries/Handlers/GetCarsByIdQueryHandler.cs
+++ CarRental/CRCQRS.Application/Queries/Handlers/GetCarsByIdQueryHandler.cs
@@ -1,10 +1,12 @@
-
 using CRCQRS.Application.Queries;
 using CRCQRS.Common;
 using CRCQRS.Infrastructure;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace AppCOG.Application.Queries.Handlers
 {
   public class GetCarsByIdCarsQueryHandler : IRequestHandler<GetCarsByIdQuery, ResponseResult>
@@ -20,17 +22,23 @@ namespace AppCOG.Application.Queries.Handlers
     {
       var response = new ResponseResult();
 
-      var cars = _context.Cars.FirstOrDefaultAsync(c => c.CarId==request.CarId, cancellationToken);
-      if(cars == null) {
+      // Use await to properly handle asynchronous operations
+      var car = await _context.Cars.FirstOrDefaultAsync(c => c.CarId == request.CarId, cancellationToken);
+
+      if (car == null)
+      {
         response.Success = false;
-        response.Message = "Invalid Car";
-       
+        response.Message = "Car not found";
+        response.StatusCode = HttpStatusCode.NotFound;
+        response.Data = null;
       }
-      response.Success = true;
-      response.Message = "User Get Car By Id successfully";
-      response.StatusCode = HttpStatusCode.OK;
-      response.Data = cars;
-    
+      else
+      {
+        response.Success = true;
+        response.Message = "Car GET By Id successfully";
+        response.StatusCode = HttpStatusCode.OK;
+        response.Data = car;
+      }
 
       return response;
     }
